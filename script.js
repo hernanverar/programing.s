4469e45f8c84158c7affc77382764a8d3e0ead6b
hernan verar <hernanverar@gmail.com>
2023-01-13 15:09:15 -0800

    WIP stuck on branching javascrip doc.events

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-// User Interface Logic
-window.onload = function() {
-    let h1 = document.querySelector("h1");
-    h1.onmouseover = function() {
-    ;
-  
-    let p = document.querySelector("p");
-    p.onmouseover = function() {
-    document.querySelector("p>em").innerText = "hello";
-    };
-  
-    window.addEventListener("load", function() {
-      const form = document.getElementById("car");
-      form.addEventListener("submit");
-    {
-        document.getElementById("car");
-      
-    
-      console.log
-    };
+window.onload = funtion=(){ 
+  let form = document.querySelector("form");
+  let resetBtn = document.getElementById("from");
+  form.onsubmit = funtion(event) {
+ document.querySelector("submit").innerText="car";
+
+ event.preventDefault();
+
+  };r
+};
