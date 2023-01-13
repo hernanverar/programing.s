2ec2bfa8def4f989dc662308eab3cf8cbd87ef04
hernan verar <hernanverar@gmail.com>
2023-01-13 13:59:05 -0800

    WIP trying to add java funtions to my html

--- script.js
+++ script.js
@@ -9,24 +9,12 @@ window.onload = function() {
     document.querySelector("p>em").innerText = "hello";
     };
   
-    document.querySelector("form").onsubmit = function(event) {
-        hideResults();
-        event.preventDefault();
-        const age = parseInt(document.querySelector("input#age").value);
-        const height = parseInt(document.querySelector("input#height").value);
+    window.addEventListener("load", function() {
+      const form = document.getElementById("car");
+      form.addEventListener("submit");
+    {
+        document.getElementById("car");
+      
     
-        if (age >= 12 && height >= 60) {
-          document.getElementById("swings").removeAttribute("class");
-          document.getElementById("coaster").removeAttribute("class");
-          document.getElementById("tower").removeAttribute("class");
-        } else if (age >= 12 || height >= 48) {
-          document.getElementById("swings").removeAttribute("class");
-          document.getElementById("coaster").removeAttribute("class");
-          // Next, we're evaluating the Swings ride.
-        } else if (age >= 6) {
-          document.getElementById("swings").removeAttribute("class");
-          // Finally, we have our "catch-all" for when a user is less than 6 years old.
-        } else {
-          document.getElementById("sorry").removeAttribute("class");
-        }
-      };
+      console.log
+    };
