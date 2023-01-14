699c2356a06dfcc4645cc932c3ff50866cbd31c9
hernan verar <hernanverar@gmail.com>
2023-01-13 17:00:52 -0800

    WIP working on hidden button options

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ window.onload = function() {
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
     // in this section we get the value for each form input
-    const p1 = document.getElementById("p1").value;
-    const p2 = document.getElementById("p2").value;
+    const p1Input = document.getElementById("p1").value;
+    const p2Input = document.getElementById("p2").value;
 
-
+    document.querySelector("span#p1").innerText = p1;
+    document.querySelector("span#p1").innerText = p2;
 
 
 
   event.preventDefault();
-
+  console.log
   };r
 };
